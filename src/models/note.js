efbdccdd955a2c1d03acfbd332cda4663da99acb
VitaliyKalamuniak <vitalikkalamunak@gmail.com>
2025-10-12 17:01:03 +0300

    add-vilidation

--- src/models/note.js
+++ src/models/note.js
@@ -36,4 +36,6 @@ const noteSchema = new Schema(
   },
 );
 
+noteSchema.index({ title: "text", content: "text", tag: "text"});
+
 export const Note = model('Note', noteSchema);
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,16 +1,21 @@
 import { Router } from 'express';
-import { getAllNotes, getNoteById, createNote, deleteNote, updateNote } from '../controllers/notesController.js';
+import { getNoteById, createNote, deleteNote, updateNote } from '../controllers/notesController.js';
+import { celebrate } from 'celebrate';
+
+import { noteIdSchema } from '../validations/notesValidation.js';
+import { getAllNotesSchema } from '../validations/notesValidation.js';
+import { getNotes } from '../controllers/notesController.js';
 
 const router = Router();
 
-router.get('/notes', getAllNotes);
+router.get('/notes', celebrate(getAllNotesSchema), getNotes);
 
-router.get('/notes/:noteId', getNoteById);
+router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
 
 router.post('/notes', createNote);
 
-router.delete('/notes/:noteId', deleteNote);
+router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
 
-router.patch('/notes/:noteId', updateNote);
+router.patch('/notes/:noteId', celebrate(noteIdSchema), updateNote);
 
 export default router;
