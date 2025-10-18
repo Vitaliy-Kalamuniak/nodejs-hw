7e22886dabcc43d347d05b88fc3a8da7b2753f68
VitaliyKalamuniak <vitalikkalamunak@gmail.com>
2025-10-18 16:11:10 +0300

    files-change

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ import authRoutes from './routes/authRoutes.js';
 import cookieParser from "cookie-parser";
 
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 
 app.use(logger);
 app.use(express.json({
